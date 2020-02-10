d7bab8e89ad6ce967b353b3ff58acab5afde5d3a
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-02-10 14:35:25 +0000

    Logbook 2 Task 3

--- Logbook2_3.php
+++ Logbook2_3.php
@@ -1,6 +1,6 @@
 <?php
 	define("PASS_GRADE",40);
-	echo "You need "" . PASS_GRADE . " % or more to pass. <br/>";
+	echo "You need " . PASS_GRADE . "% or more to pass. <br/>";
 	$grade = PASS_GRADE  + 15;
 	echo "To achieve a merit you need $grade %  or more <br/>";
 	$grade = PASS_GRADE  + 30;
