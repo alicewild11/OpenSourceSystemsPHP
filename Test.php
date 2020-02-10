d291a8a0cc7b7b8c9065945b588ce8e005008aa9
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-02-10 14:02:32 +0000

    Logbook 2 Task 1

--- Test.php
+++ Test.php
@@ -3,10 +3,9 @@
 <?php
 $hourlyrate = 5.75;
 $hoursperweek = 40;
-$gross = $hourlyrate * $hoursperweek; 
+$gross = $hourlyrate * $hoursperweek;
 $netwage = $gross * 0.78;
 echo $netwage;
-
 ?>
 </body>
 </html>
