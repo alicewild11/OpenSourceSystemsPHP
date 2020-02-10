187a83a5a5019bfa6052c8e8a02d1e24c3a1e1e7
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-02-10 14:57:25 +0000

    Update

--- Logbook2_8.php
+++ Logbook2_8.php
@@ -1,11 +1,11 @@
 <?php
   $topModules[0] = "Internet Systems Development";
   $topModules[1] = "Programming 1";
-  $topModules[2] = "Programming 2”;
+  $topModules[2] = "Programming 2";
   $topModules[3] = "OOAD";
   $topModules[4] = "Software Engineering";
   $topModules[5] = "Open Source Systems";
-  $topModules[6] = "OOSD"; 
+  $topModules[6] = "OOSD";
 
   for($count = 0,$count < 5;$count++)
   {
