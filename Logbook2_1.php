5ae5f314c1bc4d7e35f25e54a07eefef38e10cd1
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-02-10 14:12:54 +0000

    Logbook 2 Task 1

--- Logbook2_1.php
+++ Logbook2_1.php
@@ -1,3 +1,11 @@
-<?php
-echo "hello";
+<?
+$hourlyrate = 5.75;
+$hoursperweek = 40;
+$gross = $hourlyrate * $hoursperweek;
 ?>
+<html>
+<head></head>
+<body>
+  <p> My gross wage is <?php print("$gross");?>
+</body>
+</html>
