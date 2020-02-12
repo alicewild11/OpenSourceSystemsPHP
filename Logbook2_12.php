840baf23eda64f7497257c0e89d26506c9fd9c74
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-02-12 13:05:28 +0000

    Logbook 2 Task 12

--- Logbook2_12.php
+++ Logbook2_12.php
@@ -1,21 +1,18 @@
 <?
-  $topmodules[0] = "Open Source Systems ";
-  $topmodules[1] = "Programming 1";
-  $topmodules[2] = "Programming 2";
-  $topmodules[3] = "Web Application Development";
-  $topmodules[4] = "Software Engineering";
-
+  $mymarks["year 1"] = 55;
+  $mymarks["year 2"] = 65;
+  $mymarks["year 3"] = 75;
 ?>
 <head>
 <title>Data in table</title>
 </head>
 <body>
 <table border=1 align="center">
-<tr><th>Index</th><th>Subject</th></tr>
+<tr><th>Index</th><th>Marks</th></tr>
 <?
   for($count = 0;$count < 5;$count++)
   {
-    echo "<tr><td>$count</td><td>$topmodules[$count]</td></tr>";
+    echo "<tr><td>$count</td><td>$mymarks[$count]</td></tr>";
   }
 ?>
 </table>
