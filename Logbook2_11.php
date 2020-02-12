4196717973272b10d760b38e933c1d39fdffa7c1
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-02-12 12:30:32 +0000

    Logbook 2 Task 11

--- Logbook2_11.php
+++ Logbook2_11.php
@@ -1,4 +1,8 @@
 <?php
+$total = 0;
+$total = $total + $mymarks[$index];
+$average = $total /  6;
+
 $mymarks["CO556"]= 73;
 $mymarks["C0678"]= 80;
 $mymarks["CO996"]= 58;
@@ -9,8 +13,6 @@ while (list($index,$value) = each($mymarks))
   echo "for $index my grade was $value <br/>"
 };
 
-$total = 0;
-$total = $total + $mymarks[$index];
-$average = $total /  6;
+
 echo "<br/> My average score is $average";
 ?>
