0432fec6e5f2a31b2c40eef9b94ac71f43f0ee5a
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-02-17 16:08:05 +0000

    Logbook 3 Task 1

---
+++ Logbook3_1.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>Response to form</title>
+</head>
+<body>
+<?
+
+	echo "Your name is $_POST[txtname]";
+	echo "Your gender is $_POST[radsex]";
+	echo "Your occupation is $_POST[seloccupation]”;
+
+?>
+</body>
+</html>
