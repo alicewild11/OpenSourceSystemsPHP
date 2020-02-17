4f29781c6a52ba3b7da32e50d9874c2b7bfee80f
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-02-17 16:45:31 +0000

    Update

--- Logbook3_3.php
+++ Logbook3_3.php
@@ -2,13 +2,13 @@
   switch ($_POST["radcourse"])
   {
     case "HNC" : echo "You have selected an HNC course";
-                 break;
+                 
     case "HND" : echo "You have selected an HND course";
-                 break;
+
     case "BSC" : echo "You have selected a BSC course";
-                 break;
+
     case "PartTimeBSC" : echo "You have selected a Part Time BSC course";
-                         break;
+
     default    : echo "No course selected";
   }
 ?>
