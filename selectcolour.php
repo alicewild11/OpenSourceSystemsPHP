cb6e7fd2ed5fc05233d63a1467af018048ed082b
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-02-24 14:32:27 +0000

    Update

--- selectcolour.php
+++ selectcolour.php
@@ -4,14 +4,15 @@
       <form action="confirmation.php"  method="post">
 	Select the colour for the <?php echo $_POST["selqty"] ?> widgets you are ordering
    	<select name="selcolour">
-      <input type=“hidden” name=“selqty” value="?php echo $_POST["qty"]?>"/>
    	  <option>white</option>
 	  <option>red</option>
 	  <option>yellow</option>
 	  <option>green</option>
 	  <option>blue</option>
         </select>
+        <br></br>
         <input type="submit" value="Buy"/>
       </form>
+      <input type=“hidden” name=“selqty” value="?php echo $_POST["qty"]?>"/>
    </body>
 </html>
