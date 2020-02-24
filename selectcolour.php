8fe371b9b4a6e372bfa65f38b0f562c5eab789cb
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-02-24 14:15:35 +0000

    Update

--- selectcolour.php
+++ selectcolour.php
@@ -10,6 +10,7 @@
 	  <option>green</option>
 	  <option>blue</option>
         </select>
+        <input type=“hidden” name=“qty” value="<?php echo $_POST["selqty"] ?>“ />
         <br/><br/>
         <input type="submit" value="Buy"/>
       </form>
