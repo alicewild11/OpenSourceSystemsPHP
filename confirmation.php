ddb2b4db0b7c889c0bc732387a0b00b794a8d6c7
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-02-24 15:03:06 +0000

    Update

--- confirmation.php
+++ confirmation.php
@@ -1,4 +1,4 @@
 <?php
-   echo "Your order qty is $_COOKIE['selectQuantity']</br>";
+   echo "Your order qty is $_COOKIE[selectQuantity]</br>";
    echo "<h2> and the selected colour is $_POST[selcolour].</h2>";
 ?>
