9fead350b6ba748e3985c85930eb49a855efc917
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-02-28 15:10:48 +0000

    Update

--- confirmation.php
+++ confirmation.php
@@ -2,6 +2,6 @@
    session_start();
    echo "Your order quantity is $_SESSION[selectQuantity]";
    echo " and the selected colour is $_POST[selcolour].<br>";
-   echo " widget size $_COOKIE['widgetSize'] ;"
+   echo " widget size" $_COOKIE['widgetSize'] ;
 
 ?>
