5c5fe7faf4832e4ec0cbce93b9d8c914ed4b2c7b
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-02-28 15:09:19 +0000

    Update

--- confirmation.php
+++ confirmation.php
@@ -2,6 +2,6 @@
    session_start();
    echo "Your order quantity is $_SESSION[selectQuantity]";
    echo " and the selected colour is $_POST[selcolour].<br>";
-   $Price = $_SESSION['selectQuantity'] * $_COOKIE['widgetPrice'] ;
-   echo "The price of $_SESSION[selectQuantity] $_POST[selcolour] widgets is £$Price";
+   echo " widget size $_COOKIE['widgetSize'] ;"
+
 ?>
