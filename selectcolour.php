2a282da0f13f4bff7c8c5d044725fcee3cc8a287
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-02-28 14:29:51 +0000

    Update

--- selectcolour.php
+++ selectcolour.php
@@ -1,6 +1,8 @@
 <? session_start(); ?>
 <? echo "Your quantity is $_POST[selqty]";
 $_SESSION['selectQuantity'] = $_POST["selqty"]; ?>
+<? $cookieName = 'widgetPrice'; setcookie($cookieName, $_POST['txtprice']); ?>
+<? echo "Your account is $_POST['txtprice']"; ?>
 <html>
   <head><title>Select colour page</title></head>
     <body>
