5e6a54157921194b091017a200497b2f188b7d18
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-03-02 14:04:36 +0000

    Update

--- class_lip.php
+++ class_lip.php
@@ -2,10 +2,10 @@
   class person{
     var $name;
     function set_name($new_name){
-      $this->name = $new_name
+      $this->name = $new_name;
       }
     function get_name() {
-  			return $this->name;
+  			return $this-&gt;name;
   		}
   }
 ?>
