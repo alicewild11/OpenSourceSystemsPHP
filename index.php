fcc959745a32f92e9d418b4449d2ed7680af6153
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-03-02 14:04:42 +0000

    Update

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
 <!--?php include("class_lib.php"); ?-->
+<!--?php
+	$stefan = new person();
+	$jimmy = new person;
+
+	$stefan--->set_name("Stefan Mischook");
+	$jimmy-&gt;set_name("Nick Waddles");
+
+	echo "Stefan's full name: " . $stefan-&gt;get_name();
+	echo "Nick's full name: " . $jimmy-&gt;get_name();
+?&gt;
