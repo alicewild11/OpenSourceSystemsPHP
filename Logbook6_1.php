fa7f086d8a9fb820edb54b23d086f21bc9bf44c7
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-03-09 13:50:02 +0000

    Logbook 6 Task 1

---
+++ Logbook6_1.php
@@ -0,0 +1,17 @@
+<?php
+	$sql = "INSERT INTO test (name,email,phone_number)";
+	$sql = $sql . " values ('$_POST[txtName]','$_POST[txtEmail]','$_POST[txtPhoneNumber]’)";
+
+	// Connect to server and select database
+
+	// Execute sql statement
+
+	$sql = "SELECT * from test";
+
+	// Execute sql statement
+
+	while ($row = mysqli_fetch_assoc($result))
+	{
+		echo "$row[name]  $row[email]  $row[phone_number] <br/>";
+	}
+?>
