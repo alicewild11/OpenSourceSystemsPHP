79c18efeeb45dc3f4c6b70d4b834599e79a33388
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-03-09 15:02:00 +0000

    Update

--- Logbook6_1.php
+++ Logbook6_1.php
@@ -4,9 +4,10 @@
 
   $link = mysqli_connect("localhost", "root", "", "db1_alicewild");
 
-  $result = mysqli_query($link,$sql);
 
   $sql = "SELECT * from test";
+
+  $result = mysqli_query($link,$sql);
 
 	while ($row = mysqli_fetch_assoc($result))
 	{
