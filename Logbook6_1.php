2178c527af857145f325f3dfb7dd081e79522a8c
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-03-09 14:43:45 +0000

    Update

--- Logbook6_1.php
+++ Logbook6_1.php
@@ -2,14 +2,16 @@
   $sql = "INSERT INTO test (name,email,phonenumber)";
   $sql = $sql . " values ('$_POST[txtName]','$_POST[txtEmail]','$_POST[txtPhoneNumber]’)";
 
-  $sql = "SELECT * from test";
 
   $link = mysqli_connect("localhost", "root", "", "db1_alicewild");
 
   $result = mysqli_query($link,$sql);
 
+  $sql = "SELECT * from test";
+
 	while ($row = mysqli_fetch_assoc($result))
 	{
 		echo "$row[name]  $row[email]  $row[phonenumber] <br/>";
-	}
+	};
+
 ?>
