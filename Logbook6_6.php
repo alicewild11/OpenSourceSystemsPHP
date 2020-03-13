ebe087093e5dd29cbb882fa5fc58882214709d1a
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-03-13 12:26:44 +0000

    Update

--- Logbook6_6.php
+++ Logbook6_6.php
@@ -1,6 +1,6 @@
 <?php
 	include("myfunctions.inc");
 	html_header("My second function demo");
-	echo "I pay £ " . calculatetax(19994,32) . " tax";
+	echo "I pay £ " . calculatetax(15000) . " tax";
 	html_footer();
 ?>
