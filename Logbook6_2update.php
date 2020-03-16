abf40c5738224c0934e0f042bb778bc72d5e72e3
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-03-16 20:36:18 +0000

    Update

--- Logbook6_2update.php
+++ Logbook6_2update.php
@@ -1,8 +1,6 @@
 <?php
 
-$sql = "UPDATE test SET $row[phonenumber]= $txttelno WHERE name = '$_GET[id]'";
-$sql = "UPDATE test SET $row[email] = $txtemail WHERE name = '$_GET[id]'";
-
+$sql = "UPDATE test SET email = '$_POST[txtemail]', phonenumber= '$_POST[txttelno]'  WHERE ID = '$_GET[id]'";
 
   $link = mysqli_connect("localhost", "root", "", "db1_alicewild");
 
