133775a11b9fdcdc5ab73275e2da981f50892032
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-03-16 20:36:13 +0000

    Update

--- Logbook6_2action.php
+++ Logbook6_2action.php
@@ -2,13 +2,13 @@
 
 $link = mysqli_connect("localhost", "root", "", "db1_alicewild");
 
-	$sql = "SELECT * from test where name = '$_GET[id]' ";
+	$sql = "SELECT * from test where ID = '$_GET[id]' ";
 	$result = mysqli_query($link,$sql);
 	$row = mysqli_fetch_assoc($result);
 ?>
 <html>
 <body>
-<form action='Logbook6_2update.php' method='post'>
+<form action='Logbook6_2update.php?id=<?php echo $_GET['id']?>' method='post'>
 
 	Name :
 	<input type=text name=txtname value="<? echo $row['name'] ?>" readonly />
