a59d6fe041adee61dc23e2f021df383bef4c5edc
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-03-25 17:42:39 +0000

    Update

--- Logbook8_1.php
+++ Logbook8_1.php
@@ -1,19 +1,19 @@
 <?php
-     $lottodate = date("dmY");
+     $lottodate = date("d m Y");
      echo "The lottery numbers for $lottodate are ";
      for($n=1;$n<7;$n++){
         $number[$n] = rand(1,49);
         echo "<br/> $number[$n]";
      }
 
-     $conn = mysqli_connect("localhost", "username", "password", "databasename");
+     $conn = mysqli_connect("localhost", "root", "", "db1_alicewild");
 
      $sql = "insert into lotto (lottodate,number1,number2,";
      $sql = $sql . "number3,number4,number5,number6)";
      $sql = $sql . " values ($lottodate,$number[1],$number[2],";
      $sql = $sql . "$number[3],$number[4],$number[5],$number[6]);";
 
-     $result = mysqli_query($sql);
+     $result = mysqli_query($conn,$sql);
      echo "<br/>This weeks numbers have been saved";
 
 ?>
