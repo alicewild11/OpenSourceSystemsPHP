e8294ddb263d4cf882976451c8c6845169236aec
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-03-25 18:05:09 +0000

    Update

--- Logbook8_1.php
+++ Logbook8_1.php
@@ -8,12 +8,12 @@
 
      $conn = mysqli_connect("localhost", "root", "", "db1_alicewild");
 
-     $sql = "insert into lotto (lottodate,number1,number2,";
+     $sql = "INSERT INTO Lotto (lottodate,number1,number2,";
      $sql = $sql . "number3,number4,number5,number6)";
      $sql = $sql . " values ($lottodate,$number[1],$number[2],";
      $sql = $sql . "$number[3],$number[4],$number[5],$number[6]);";
 
-     $result = mysqli_query($conn,$sql);
+     $result = mysqli_query($conn, $sql);
      echo "<br/>This weeks numbers have been saved";
 
 ?>
