f811bd002cb7a240f43cde22b4bf4ab78eca0496
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-04-14 01:10:29 +0100

    Save Monster

--- savemonster.php
+++ savemonster.php
@@ -1,20 +1,18 @@
 <?php
 
-$db = mysqli_connect("localhost", "root", "", "db1_alicewild");
+  $db = mysqli_connect("localhost", "root", "", "db1_alicewild");
 
-// Obtain the file sent to the server within the response.
-$image = $_FILES['monsterimage']['tmp_name'];
-$audio = $_FILES['monsteraudio']['tmp_name'];
+  // Obtain the file sent to the server within the response.
+  $image = $_FILES['monsterimage']['tmp_name'];
+  $audio = $_FILES['monsteraudio']['tmp_name'];
 
   // Get the file binary data
   $imagedata = addslashes(fread(fopen($image, "r"), filesize($image)));
   $audiodata = addslashes(fread(fopen($audio, "r"), filesize($audio)));
 
-  $sql = "INSERT INTO Monster";
-  $sql .= "(Name, Image, Audio) ";
-  $sql .= "VALUES ('$_POST[txtname]', '$imagedata','$audiodata');";
+  $sql = "INSERT INTO Monster (Name, Image, Audio) VALUES ('$_POST[txtname]', '$imagedata','$audiodata')";
 
   $result = mysqli_query($db, $sql);
 
-   mysqli_close($db);
+  mysqli_close($db);
 ?>
